5c86b95c496e580fdfab5a3e7a757e682fe00d09
KasiahButler <snipercorgii@gmail.com>
2018-11-26 16:25:06 -0800

    Adding Some Base Files
    Base Files for Tile, TileDef, GI, and DM

---
+++ Assets/Scripts/DataManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ResourceType
+{
+	Tile = 0,
+	TileDef = 1
+}
+
+public class DataManager
+{
+	private Dictionary<string, TileDef> tileDefs;
+	private Dictionary<string, Tile> tiles;
+
+	private bool initialized = false;
+
+	public DataManager()
+	{
+		InitData();
+	}
+
+	private void InitData()
+	{
+		tileDefs = new Dictionary<string, TileDef>();
+		tiles = new Dictionary<string, Tile>();
+		initialized = true;
+	}
+
+	public object Get(ResourceType type, string id)
+	{
+		if(!initialized)
+		{
+			return null;
+		}
+
+		switch(type)
+		{
+			case ResourceType.Tile:
+				Tile tile;
+				tiles.TryGetValue(id, out tile);
+				return tile;
+			case ResourceType.TileDef:
+				TileDef tileDef;
+				tileDefs.TryGetValue(id, out tileDef);
+				return tileDef;
+			default:
+				Debug.Log(string.Format("DataManager failed to find {0}", id));
+				return null;
+		}
+	}
+}
---
+++ Assets/Scripts/TileDef.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ActionType
+{
+	Move = 0,
+	Jump = 1,
+	Slide = 2,
+	Jumpslide = 3,
+	Strike = 4,
+	Command = 5
+}
+
+public class ActionDef
+{
+	public ActionType type { get; private set; }
+
+	public Vector2 target { get; private set; }
+}
+
+public class TileDef
+{
+	public string ID { get; private set; }
+	public string DisplayName { get; private set; }
+	public string PrefabID { get; private set; }
+
+	public string AbilityID { get; private set; }
+	public List<ActionDef> Actions { get; private set; }
+}
