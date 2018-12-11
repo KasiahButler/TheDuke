62d0078d882f62fb4393f1abaabbbda803656e66
Talon <zinger321321@gmail.com>
2018-12-10 18:46:17 -0800

    MainMenu Script Added
    Added Script to control the Play Button on main menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    //Sets the playGameIndex to 1 to signify loading of scene 1 based on build manager
+    private int playGameIndex = 1;
+
+    //This is what we are going to call when we click on our button
+	public void PlayGame()
+    {
+        SceneManager.LoadScene(playGameIndex);
+    }
+}
